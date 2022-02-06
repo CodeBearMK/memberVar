734e17e3594fd44bf53ab833a13468c07955027c
cat32tw <cat32tw@SWEETLIFE>
2022-02-06 12:30:08 +0800

    加入專案檔案。

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace memberVar
+{
+    class Program
+    {
+        
+        private static int n = 5; //成員變數(欄位變數)
+
+        //--------------------------------------------------
+        private static void Add()
+        {
+            n += 5; //n=15+5=20
+            Console.WriteLine(" n = {0} ", n); //顯示 n=20
+        }
+        //--------------------------------------------------
+
+        static void Main(string[] args)
+        {
+            n += 10; //n=5+10=15
+            Console.WriteLine(" n = {0} ", n); //顯示n=15
+            Add();
+            n += 20; //n=20+20=40
+            Console.WriteLine(" n = {0} ", n); //顯示n=40
+            Console.Read();
+        }
+
+    }
+}
